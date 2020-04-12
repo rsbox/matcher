dc86095b8c8f0abfc2cf069f9998dcdaa4da6182
kyle-escobar <kslrtips@gmail.com>
2020-04-11 21:40:34 -0400

    Add new project windows logic

--- gui/src/main/kotlin/osrs/rsbox/matcher/gui/window/NewProjectWindow.kt
+++ gui/src/main/kotlin/osrs/rsbox/matcher/gui/window/NewProjectWindow.kt
@@ -2,7 +2,10 @@ package osrs.rsbox.matcher.gui.window
 
 import javafx.beans.property.SimpleStringProperty
 import javafx.geometry.Pos
+import javafx.scene.control.Alert
 import javafx.scene.control.ListView
+import javafx.scene.control.SelectionMode
+import javafx.stage.FileChooser
 import osrs.rsbox.matcher.gui.controller.NewProjectController
 import tornadofx.*
 import java.io.File
@@ -23,6 +26,12 @@ class NewProjectWindow : View("Create New Project") {
      */
     private val newProjectController: NewProjectController by inject()
 
+    /**
+     * Observables
+     */
+    private val inputJars = mutableListOf<File>().asObservable()
+    private val referenceJars = mutableListOf<File>().asObservable()
+
     override val root = Form()
 
     init {
@@ -41,10 +50,17 @@ class NewProjectWindow : View("Create New Project") {
                         prefHeight = 50.0
 
                         model.inputJarList = this
+
+                        selectionModel.selectionMode = SelectionMode.SINGLE
+
+                        inputJars.onChange {
+                            items.setAll(it.list)
+                        }
+
                     }
                     hbox(1) {
-                        button("Add")
-                        button("Remove")
+                        button("Add").action { addJarFile(Target.INPUT) }
+                        button("Remove").action { removeSelectedJars(Target.INPUT) }
                     }
                 }
 
@@ -54,10 +70,16 @@ class NewProjectWindow : View("Create New Project") {
                         prefHeight = 50.0
 
                         model.referenceJarList = this
+
+                        selectionModel.selectionMode = SelectionMode.SINGLE
+
+                        referenceJars.onChange {
+                            items.setAll(it.list)
+                        }
                     }
                     hbox(1) {
-                        button("Add")
-                        button("Remove")
+                        button("Add").action { addJarFile(Target.REFERENCE) }
+                        button("Remove").action { removeSelectedJars(Target.REFERENCE) }
                     }
                 }
             }
@@ -69,6 +91,48 @@ class NewProjectWindow : View("Create New Project") {
                 button("Create")
             }
         }
+    }
+
+    /**
+     * Opens a file selection window and returns a single [File]
+     */
+    private fun openJarChooser(): File {
+        return chooseFile("Choose Jar", mode = FileChooserMode.Single, filters = arrayOf(FileChooser.ExtensionFilter("jar", "*.jar"))).first()
+    }
+
+    private fun addJarFile(target: Target) {
+        val targetObservable = when(target) {
+            Target.INPUT -> inputJars
+            Target.REFERENCE -> referenceJars
+        }
+
+        if(targetObservable.size > 0) {
+            alert(Alert.AlertType.ERROR, "Oh Snap!", "You can only have one $target jar selected.")
+            return
+        }
+
+        val jarFile = this.openJarChooser()
+        targetObservable.add(jarFile)
+    }
+
+    private fun removeSelectedJars(target: Target) {
+        val targetObservable = when(target) {
+            Target.INPUT -> inputJars
+            Target.REFERENCE -> referenceJars
+        }
+
+        val targetList = when(target) {
+            Target.INPUT -> model.inputJarList
+            Target.REFERENCE -> model.referenceJarList
+        }
+
+        val selections = targetList.selectionModel.selectedItems
+        targetObservable.removeAll(selections)
+    }
+
+    private enum class Target {
+        INPUT,
+        REFERENCE;
     }
 
     /**
