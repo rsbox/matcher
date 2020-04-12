d8d4aa9d1a5c4bf56be9e4852e1018b9220db0dc
kyle-escobar <kslrtips@gmail.com>
2020-04-11 21:22:18 -0400

    Add new project interface components

---
+++ gui/src/main/kotlin/osrs/rsbox/matcher/gui/controller/NewProjectController.kt
@@ -0,0 +1,9 @@
+package osrs.rsbox.matcher.gui.controller
+
+import tornadofx.Controller
+
+/**
+ * New project controller actions.
+ */
+class NewProjectController : Controller() {
+}
--- gui/src/main/kotlin/osrs/rsbox/matcher/gui/window/NewProjectWindow.kt
+++ gui/src/main/kotlin/osrs/rsbox/matcher/gui/window/NewProjectWindow.kt
@@ -1,7 +1,11 @@
 package osrs.rsbox.matcher.gui.window
 
-import tornadofx.Form
-import tornadofx.View
+import javafx.beans.property.SimpleStringProperty
+import javafx.geometry.Pos
+import javafx.scene.control.ListView
+import osrs.rsbox.matcher.gui.controller.NewProjectController
+import tornadofx.*
+import java.io.File
 
 /**
  * The new project window / dialog.
@@ -9,11 +13,74 @@ import tornadofx.View
  */
 class NewProjectWindow : View("Create New Project") {
 
+    /**
+     * The new project model
+     */
+    private val model = NewProjectModel()
+
+    /**
+     * Controllers
+     */
+    private val newProjectController: NewProjectController by inject()
+
     override val root = Form()
 
     init {
         with(root) {
 
+            fieldset("Project Name") {
+                textfield(model.projectName) {
+                    prefWidth = 500.0
+                }
+            }
+
+            hbox(5) {
+                fieldset("Input Jar") {
+                    listview<File> {
+                        prefWidth = 245.0
+                        prefHeight = 50.0
+
+                        model.inputJarList = this
+                    }
+                    hbox(1) {
+                        button("Add")
+                        button("Remove")
+                    }
+                }
+
+                fieldset("Reference Jar") {
+                    listview<File> {
+                        prefWidth = 245.0
+                        prefHeight = 50.0
+
+                        model.referenceJarList = this
+                    }
+                    hbox(1) {
+                        button("Add")
+                        button("Remove")
+                    }
+                }
+            }
+
+            hbox(5) {
+                alignment = Pos.CENTER_RIGHT
+
+                button("Cancel").action { close() }
+                button("Create")
+            }
         }
+    }
+
+    /**
+     * Private model class
+     */
+    private class NewProjectModel {
+        /**
+         * The name of the project.
+         */
+        val projectName = SimpleStringProperty()
+
+        var inputJarList: ListView<File> by singleAssign()
+        var referenceJarList: ListView<File> by singleAssign()
     }
 }
